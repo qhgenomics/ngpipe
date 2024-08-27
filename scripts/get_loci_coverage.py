792e19663e9aa8e6da4b150ee05c3a65227fbefa
mjsull <mjsull@gmail.com>
2024-08-27 15:56:02 +1000

    fix typo

--- scripts/get_loci_coverage.py
+++ scripts/get_loci_coverage.py
@@ -4,7 +4,7 @@ import os
 
 
 def create_coverage_files(input_file, scheme, mlst_dir, outfile):
-    with (open(input_file) as f, open("data/step5_cov/{}.{}.fasta".format(snakemake.wildcards.sample, scheme), "w") as o):
+    with (open(input_file) as f, open("step5_cov/{}.{}.fasta".format(snakemake.wildcards.sample, scheme), "w") as o):
         splitline = f.readline().rstrip().split("\t")
         contig, scheme, profile = splitline[:3]
         alleles = splitline[3:]
@@ -25,14 +25,14 @@ def create_coverage_files(input_file, scheme, mlst_dir, outfile):
                     elif getseq:
                         o.write(line)
     if params.read_dir != "none":
-        subprocess.Popen("minimap2 -ax sr data/step5_cov/{sample}.{scheme}.fasta {read_dir}/{sample}_R1.fastq.gz {readir}/{sample}_R2.fastq.gz"
-                         " | samtools view -bS - | samtools sort -o data/step5_cov/{sample}.{scheme}.bam && "
-                         " samtools depth -aa data/step5_cov/{sample}.{scheme}.bam > {coverage}".format(
+        subprocess.Popen("minimap2 -ax sr step5_cov/{sample}.{scheme}.fasta {read_dir}/{sample}_R1.fastq.gz {readir}/{sample}_R2.fastq.gz"
+                         " | samtools view -bS - | samtools sort -o step5_cov/{sample}.{scheme}.bam && "
+                         " samtools depth -aa step5_cov/{sample}.{scheme}.bam > {coverage}".format(
             sample=snakemake.wildcards.sample, scheme=scheme, read_dir=snakemake.params.read_dir, cov=outfile), shell=True).wait()
     else:
-        subprocess.Popen("minimap2 -ax asm5 data/step5_cov/{sample}.{scheme}.fasta {contig_dir}/{sample}.fasta"
-                         " | samtools view -bS - | samtools sort -o data/step5_cov/{sample}.{scheme}.bam && "
-                         " samtools depth -aa data/step5_cov/{sample}.{scheme}.bam > {coverage}".format(
+        subprocess.Popen("minimap2 -ax asm5 step5_cov/{sample}.{scheme}.fasta {contig_dir}/{sample}.fasta"
+                         " | samtools view -bS - | samtools sort -o step5_cov/{sample}.{scheme}.bam && "
+                         " samtools depth -aa step5_cov/{sample}.{scheme}.bam > {coverage}".format(
             sample=snakemake.wildcards.sample, scheme=scheme, read_dir=snakemake.params.read_dir, cov=outfile), shell=True).wait()
 
 create_coverage_files(snakemake.input.mlst, "mlst", snakemake.input.mlst_dir, snakemake.output.mlst_cov )
